dc24ccd1665e636f4ea59dcdaa4656a49271b3d2
Amir Jann Castaneda <amir@zap.com.ph>
2022-12-02 21:36:20 +0800

    Create solution for containsDuplicate

---
+++ contains-duplicate.ts
@@ -0,0 +1,35 @@
+type ContainsDuplicate = (nums: number[]) => boolean;
+
+// Brute force approach
+const containsDuplicate1: ContainsDuplicate = (nums) => {
+  for(let i=0; i < nums.length; i++) {
+    let refNumber = nums[i];
+    for (let j=i+1; j < nums.length; j++) {
+      if(refNumber === nums[j]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+const containsDuplicate2: ContainsDuplicate = (nums) => {
+  let newSet = new Set(nums);
+  return nums.length !== newSet.size;
+}
+
+const containsDuplicate3: ContainsDuplicate = (nums) => {
+  let map = {};
+
+  for(let i=0; i < nums.length; i++) {
+    if(map[nums[i]] === undefined) {
+      map[nums[i]] = i;
+    }
+    else {
+      return true;
+    }
+  }
+
+  return false;
+}
